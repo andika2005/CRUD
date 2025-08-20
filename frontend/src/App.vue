9af0b10a885114da368a92a5942fe9b154ee963c
andika2005 <andikanasution538@gmail.com>
2025-08-20 15:05:08 +0700

    test update navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,23 @@
 <template>
   <div class="page-container">
     <!-- Navbar -->
-    <nav class="navbar is-primary" role="navigation" aria-label="main navigation">
+    <nav
+      class="navbar is-primary"
+      role="navigation"
+      aria-label="main navigation"
+    >
       <div class="navbar-brand">
         <router-link to="/" class="navbar-item">
-          My MDV-CODING-EXAM
+          javascriptGarisKeras
         </router-link>
 
-        <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+        <a
+          role="button"
+          class="navbar-burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+        >
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
@@ -15,15 +25,10 @@
       </div>
 
       <div id="navbarBasicExample" class="navbar-menu">
-        <div class="navbar-start">
-          
-
-        </div>
+        <div class="navbar-start"></div>
 
         <div class="navbar-end">
-          <router-link to="/" class="navbar-item">
-            Home
-          </router-link>
+          <router-link to="/" class="navbar-item"> Home </router-link>
           <router-link :to="{ name: 'CreateNewTask' }" class="navbar-item">
             Add new task
           </router-link>
@@ -44,5 +49,4 @@
   background-color: #ecfdf5; /* Set your desired background color */
   min-height: 100vh; /* Ensure the container takes at least the full height of the viewport */
 }
-
 </style>
