5a2f6cda65eaa150dcb55289f46de7a5d7f1cc67
andika2005 <andikanasution538@gmail.com>
2025-08-20 15:00:27 +0700

    First commit

---
+++ server/config/database.js
@@ -0,0 +1,11 @@
+const mysql = require("mysql2");
+
+//connection to task_manager_db
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Andika123",
+  database: "task_manager_db",
+});
+
+module.exports = db;
